4922a703bfa2602b19f16c0aa9d763e7f360d0f8
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-24 18:56:37 +0600

    Nav and Banner

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,9 +1,52 @@
+import { NavLink } from "react-router-dom";
+import logo from '../../../assets/logo.png'
 
 const NavBar = () => {
+    const navLinks = <>
+     <li><NavLink to="/">Item 1</NavLink></li>
+     <li><NavLink to="/2">Item 2</NavLink></li>
+     <li><NavLink to="/3">Item 3</NavLink></li>
+     <li><NavLink to="/4">Item 4</NavLink></li>
+    </>
+    
     return (
-        <div>
-            Nav
+        <div className="navbar bg-opacity-30 fixed z-10 text-white bg-black">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+             {navLinks}
+            </ul>
+          </div>
+          <NavLink to="/" className="btn btn-ghost text-xl">
+            <img className="w-10" src={logo} alt="" />
+            Bistro Boss
+          </NavLink>
         </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+           {navLinks}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
     );
 };
 
