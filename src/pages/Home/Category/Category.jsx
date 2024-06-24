0077a75db3c038a55a2baf45d274da773e7088a9
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-24 19:57:01 +0600

    Category with Swiper

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,66 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide4.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide2.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
+
+const Category = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-10 my-10">
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+          <h3 className="text-4xl text-white uppercase text-center -mt-12">
+            Salad
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+          <h3 className="text-4xl text-white uppercase text-center -mt-12">
+            Deserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+          <h3 className="text-4xl text-white uppercase text-center -mt-12">
+            Soup
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+          <h3 className="text-4xl text-white uppercase text-center -mt-12">
+            Pizza
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+          <h3 className="text-4xl text-white uppercase text-center -mt-12">
+            Salad
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+          <h3 className="text-4xl text-white uppercase text-center -mt-12">
+            Soup
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Category;
