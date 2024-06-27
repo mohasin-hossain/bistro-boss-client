025937772a01598e35882049a1d37926909c3334
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-27 10:26:48 +0600

    Conditional rendering of admin related navlinks

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { BiTask } from "react-icons/bi";
-import { FaHome, FaShoppingBag } from "react-icons/fa";
-import { FaTowerObservation } from "react-icons/fa6";
+import { FaBook, FaHome, FaList, FaShoppingBag, FaUsers, FaUtensils } from "react-icons/fa";
 import { GiWallet } from "react-icons/gi";
 import { GoCodeReview } from "react-icons/go";
 import { IoCart, IoMailOutline, IoMenu } from "react-icons/io5";
@@ -8,47 +7,89 @@ import { SlCalender } from "react-icons/sl";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
+  // TODO: get isAdmin value from the database
+  const isAdmin = true;
+
   return (
     <div className="flex">
       {/* Dashboard Sidebar */}
       <div className="w-64 min-h-screen bg-orange-400">
         <ul className="menu p-4 rounded-box space-y-2">
-          <li>
-            <NavLink to="/dashboard/userHome">
-              <FaHome className="text-2xl" />
-              User Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/reservation">
-              <BiTask  className="text-2xl" />
-              Reservation
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/payment">
-              <GiWallet className="text-2xl" />
-              Payment History
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/cart">
-              <IoCart className="text-2xl" />
-              My Cart
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/review">
-              <GoCodeReview className="text-2xl" />
-              Add Review
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/booking">
-              <SlCalender className="text-2xl" />
-              My Booking
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/adminHome">
+                  <FaHome className="text-2xl" />
+                  Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/addItems">
+                  <FaUtensils className="text-2xl" />
+                  Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageItems">
+                  <FaList className="text-2xl" />
+                  Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/bookings">
+                  <FaBook className="text-2xl" />
+                  Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/users">
+                  <FaUsers className="text-2xl" />
+                  All Users
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/userHome">
+                  <FaHome className="text-2xl" />
+                  User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservation">
+                  <BiTask className="text-2xl" />
+                  Reservation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/payment">
+                  <GiWallet className="text-2xl" />
+                  Payment History
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/cart">
+                  <IoCart className="text-2xl" />
+                  My Cart
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/review">
+                  <GoCodeReview className="text-2xl" />
+                  Add Review
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/myBooking">
+                  <SlCalender className="text-2xl" />
+                  My Booking
+                </NavLink>
+              </li>
+            </>
+          )}
+
+          {/* Shared Nav Links */}
           <div className="divider"></div>
           <li>
             <NavLink to="/">
