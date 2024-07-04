6fd27cb8760675607ad5c998d70e4170a860a268
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-07-04 22:28:22 +0600

    Ordered Items not updating in modal of myOrders issue fix-

--- src/pages/Dashboard/MyOrders/MyOrders.jsx
+++ src/pages/Dashboard/MyOrders/MyOrders.jsx
@@ -53,18 +53,22 @@ const MyOrders = () => {
                 <td>{payment.email}</td>
                 <td>{payment.transactionId}</td>
                 <td>
-                  
                   {/* User Order List Modal */}
                   <button
                     className="link text-[#D1A054] block"
                     onClick={() =>
-                      document.getElementById("my_modal_2").showModal()
+                      document
+                        .getElementById(`my_modal_${payment._id}`)
+                        .showModal()
                     }
                   >
                     View Items
                   </button>
 
-                  <dialog id="my_modal_2" className="modal modal-bottom sm:modal-middle">
+                  <dialog
+                    id={`my_modal_${payment._id}`}
+                    className="modal modal-bottom sm:modal-middle"
+                  >
                     <div className="modal-box ">
                       <h3 className="text-2xl text-center">Your Order</h3>
                       <div className="divider"></div>
@@ -78,7 +82,6 @@ const MyOrders = () => {
                       <button>close</button>
                     </form>
                   </dialog>
-
                 </td>
                 <td>${payment.price}</td>
                 <td>{moment(payment.date).format("MMMM Do YYYY, h:mm a")}</td>
