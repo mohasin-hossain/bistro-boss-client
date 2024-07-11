bbcb94d3fb0d3059d56ad069927708caf468e88f
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-07-12 00:10:53 +0600

    Loader

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,16 +1,37 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import NavBar from "../pages/Shared/NavBar/NavBar";
+import { useEffect, useState } from "react";
+import Loader from "../pages/Loader/Loader";
 
 const Main = () => {
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
-  const noHeaderFooter = location.pathname.includes("login") || location.pathname.includes("register");
+  const noHeaderFooter =
+    location.pathname.includes("login") ||
+    location.pathname.includes("register");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
     <div>
-      {noHeaderFooter || <NavBar></NavBar>}
-      <Outlet></Outlet>
-      {noHeaderFooter || <Footer></Footer>}
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {noHeaderFooter || <NavBar></NavBar>}
+          <Outlet></Outlet>
+          {noHeaderFooter || <Footer></Footer>}
+        </>
+      )}
     </div>
   );
 };
---
+++ src/pages/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import LoaderImg from "../../assets/others/cupcake.gif";
+
+const Loader = () => {
+  return (
+    <div className="grid place-items-center">
+      <img src={LoaderImg} alt="" />
+    </div>
+  );
+};
+
+export default Loader;
