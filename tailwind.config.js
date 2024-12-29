66aa8a9ef124d6423b9a4cc009724fc0248373dd
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-12-29 14:44:14 +0100

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: [
     "./index.html",
@@ -17,7 +19,7 @@ export default {
     },
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
-}
+};
 
